a5811495f9f652c6a96bc80747c5fdbd16bda98b
ludovicjj <jahanlud@gmail.com>
2019-02-11 15:09:15 +0100

    Add method addPhone and removePhone

--- src/Domain/Entity/Maker.php
+++ src/Domain/Entity/Maker.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
@@ -17,15 +18,47 @@ class Maker
     protected $name;
 
     /**
+     * @var ArrayCollection
+     */
+    protected $phones;
+
+    /**
+     * Maker constructor.
      * @param string $name
+     * @param array $phones
      * @throws \Exception
      */
     public function __construct(
-        string $name
+        string $name,
+        array $phones = []
     )
     {
         $this->id = Uuid::uuid4();
         $this->name = $name;
+        $this->phones = new ArrayCollection($phones);
+    }
+
+    /**
+     * @param array $phones
+     */
+    public function addPhone(array $phones)
+    {
+        // Defined Maker for each phone
+        foreach ($phones as $phone) {
+            $phone->definedMaker($this);
+            $this->phones[] = $phone;
+        }
+    }
+
+    /**
+     * @param array $phones
+     */
+    public function removePhone(array $phones)
+    {
+        // Delete phones in ArrayCollection
+        foreach ($phones as $phone) {
+            $this->phones->removeElement($phone);
+        }
     }
 
     public function getId(): UuidInterface

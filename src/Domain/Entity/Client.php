f8771f2bc1ba5ddd5dea954f728c6c8d4897a355
ludovicjj <jahanlud@gmail.com>
2019-02-12 13:36:37 +0100

    Client Entity implements UserInterface

--- src/Domain/Entity/Client.php
+++ src/Domain/Entity/Client.php
@@ -16,9 +16,21 @@ class Client implements UserInterface
      * @var string
      */
     protected $username;
+    /**
+     * @var string
+     */
     protected $password;
+    /**
+     * @var \DateTime
+     */
     protected $createdAt;
+    /**
+     * @var \DateTime
+     */
     protected $updatedAt;
+    /**
+     * @var array
+     */
     protected $roles;
 
     /**
@@ -30,15 +42,60 @@ class Client implements UserInterface
         $this->id = Uuid::uuid4();
     }
 
+    /**
+     * @param string $username
+     * @param string $password
+     * @throws \Exception
+     */
     public function createClient(
-        string $username
+        string $username,
+        string $password
     )
     {
         $this->username = $username;
+        $this->password = $password;
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = null;
+        $this->roles = ['ROLE_USER'];
+
     }
 
-    public function getId()
+    public function getId(): UuidInterface
     {
         return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
     }
 }

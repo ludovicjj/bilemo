dc90e0fa5f0015cf448921cdf9dd77ca9642b996
ludovicjj <jahanlud@gmail.com>
2019-02-13 13:19:14 +0100

    Events::AUTHENTICATION_SUCCESS - Adding public data to the JWT response

---
+++ src/Application/Subscribers/AuthenticationSuccessListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Application\Subscribers;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $client = $event->getUser();
+
+        if (!$client instanceof UserInterface) {
+            return;
+        }
+
+        $data['client'] = [
+            'username' => $client->getUsername(),
+            'email' => $client->getEmail(),
+            'roles' => $client->getRoles()
+        ];
+
+        $event->setData($data);
+    }
+}
